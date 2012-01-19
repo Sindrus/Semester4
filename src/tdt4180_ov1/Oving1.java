d6c496cd9043d82b964736ffcc78d94add8b87ed
Sindre Nygard <sindreny@gmail.com>
2012-01-19 15:05:06 +0100

    Hadde ikke med Oving1 fila

--- src/tdt4180_ov1/Oving1.java
+++ src/tdt4180_ov1/Oving1.java
@@ -1,27 +1,20 @@
 package tdt4180_ov1;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Oving1 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+public class Oving1{
+	
+	public static void main(String[] args){
 		
 		JFrame jf = new JFrame();
-		jf.setSize(500, 60);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setSize(600, 70);
+		JPanel bnt = new ButtonsNText();
+	
+		jf.add(bnt);
+	
 		jf.setVisible(true);
-		
-		JPanel jp = new JPanel();
-		JButton btn1 = new JButton();
-		btn1.setText("Trykk her");
-		
-		
-		jp.add(btn1);
-		jf.add(jp);
 	}
-
+	
 }
