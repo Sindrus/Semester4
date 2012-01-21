73ae85ec66951d3740df0a8541b5eb61b114559b
Sindre Nygard <sindreny@gmail.com>
2012-01-21 15:16:58 +0100

    Started gridbaglayout

--- src/tdt4180_ov2/Oving2.java
+++ src/tdt4180_ov2/Oving2.java
@@ -1,5 +1,8 @@
 package tdt4180_ov2;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
 public class Oving2 {
@@ -11,5 +14,20 @@ public class Oving2 {
 		jf.setSize(500, 500);
 		jf.add(new PersonPanel());
 		jf.setVisible(true);
+		jf.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyPressed(KeyEvent e) {}
+
+			@Override
+			public void keyReleased(KeyEvent e) {}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if(e.getKeyChar()==KeyEvent.VK_SPACE){
+					System.exit(0);
+				}
+			}
+		});
 	}
 }
--- src/tdt4180_ov2/PersonPanel.java
+++ src/tdt4180_ov2/PersonPanel.java
@@ -2,9 +2,15 @@ package tdt4180_ov2;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.ComponentOrientation;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.LayoutManager;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
@@ -23,18 +29,38 @@ public class PersonPanel extends JPanel{
 	JSlider HeightPropertyComponent;
 	
 	public PersonPanel(){
+		setLayout(new BorderLayout());
 		JLabel lblName = new JLabel("Name:");
 		JLabel lblEmail= new JLabel("Email:");
 		JLabel lblDOB = new JLabel("Birthday:");
 		JLabel lblGender = new JLabel("Gender:");
 		JLabel lblHeight = new JLabel("Height:");
 		
-		JPanel jp1 = new JPanel(new BorderLayout());
-		jp1.add(lblName, BorderLayout.AFTER_LAST_LINE);
-		jp1.add(lblEmail, BorderLayout.AFTER_LAST_LINE);
-		jp1.add(lblDOB, BorderLayout.AFTER_LAST_LINE);
-		jp1.add(lblHeight, BorderLayout.AFTER_LAST_LINE);
+//		JPanel jp1 = new JPanel();
+//		jp1.setLayout(null);
+
+		JPanel jp1 = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.gridx=0;
+		c.gridy=0;
+		jp1.add(lblName,c);
+		
+		c.gridy=1;
+		jp1.add(lblEmail,c);
+		
+		c.gridy=2;
+		jp1.add(lblDOB,c);
+		
+		c.gridy=3;
+		jp1.add(lblGender,c);
+		
+		c.gridy=4;
+//		lblHeight.setBounds(50, 200, 75, 25);
+		jp1.add(lblHeight);
+		
 		add(jp1);
 	}
+
 	
 }
