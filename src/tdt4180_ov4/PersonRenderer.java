e226201e659e5ba1955a56239d173bcb845f29e4
Sindre <sindreny@gmail.com>
2012-02-28 09:18:14 +0100

    added person renderer

---
+++ src/tdt4180_ov4/PersonRenderer.java
@@ -0,0 +1,19 @@
+package tdt4180_ov4;
+
+import java.awt.Component;
+
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class PersonRenderer implements ListCellRenderer{
+	
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public class DefaultListCellRenderer extends PersonRenderer{
+		
+	}
+}
